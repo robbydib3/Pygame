dd7f31f973681f5d9a603a3fd51edea0e229857a
robbydib3 <robertdibenedetto3@gmail.com>
2023-05-29 16:06:33 -0400

    Open a window in pygame

--- TestCode.py
+++ TestCode.py
@@ -1,1 +1,25 @@
 ## Install and setup
+import pygame
+
+pygame.init()
+
+# Set the width and height of the screen (window)
+screen_width = 800
+screen_height = 600
+
+# Create the screen (window)
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Main game loop
+running = True
+while running:
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update the screen (window)
+    pygame.display.flip()
+
+# Quit Pygame
+pygame.quit()
