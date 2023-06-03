9133944dd0e4d8716111940611302e8b0f608381
robbydib3 <robertdibenedetto3@gmail.com>
2023-06-02 23:58:25 -0400

    Got to 44:27 in the video
    Just finished adding border

--- TestCode.py
+++ TestCode.py
@@ -1,25 +1,70 @@
-## Install and setup
+# All credits to this work go to Tech With Tim on youtube
+## https://www.youtube.com/watch?v=jO6qQDNa2UY
+
 import pygame
+import os
 
-pygame.init()
+WIDTH, HEIGHT = 900,500 # Set width
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("First Game") # set window name
 
-# Set the width and height of the screen (window)
-screen_width = 800
-screen_height = 600
+white = (255,255,255) # set color
+black = (0,0,0)
+FPS = 60 # save FPS counter
+BORDER = pygame.Rect(WIDTH/2-5,0,10,HEIGHT) #in the middle of screen
 
-# Create the screen (window)
-screen = pygame.display.set_mode((screen_width, screen_height))
+#spaceships
+yellow_spaceship = pygame.image.load(os.path.join('Assets','spaceship_yellow.png'))
+yellow_spaceship = pygame.transform.rotate(pygame.transform.scale(yellow_spaceship,(55,40)),90)
+red_spaceship = pygame.image.load(os.path.join('Assets','spaceship_red.png'))
+red_spaceship = pygame.transform.rotate(pygame.transform.scale(red_spaceship,(55,40)),270)
 
-# Main game loop
-running = True
-while running:
-    # Event handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-    # Update the screen (window)
-    pygame.display.flip()
+def draw_window(red,yellow):
+    WIN.fill(white) # fill background with saved color
+    pygame.draw.rect(WIN,black,BORDER)
+    WIN.blit(yellow_spaceship,(yellow.x,yellow.y))
+    WIN.blit(red_spaceship,((red.x,red.y)))
+    pygame.display.update() # update display
 
-# Quit Pygame
-pygame.quit()
+def yellow_handle_movement(keys_pressed,yellow):
+    if keys_pressed[pygame.K_a]: # left
+        yellow.x -= 4
+    if keys_pressed[pygame.K_d]: # right
+        yellow.x += 4
+    if keys_pressed[pygame.K_w]: # up
+        yellow.y -= 4
+    if keys_pressed[pygame.K_s]: # down
+        yellow.y += 4
+
+def red_handle_movement(keys_pressed,red):
+    if keys_pressed[pygame.K_LEFT]: # left
+        red.x -= 4
+    if keys_pressed[pygame.K_RIGHT]: # right
+        red.x += 4
+    if keys_pressed[pygame.K_UP]: # up
+        red.y -= 4
+    if keys_pressed[pygame.K_DOWN]: # down
+        red.y += 4
+
+def main():
+    red = pygame.Rect(700,300,55,40)
+    yellow = pygame.Rect(100,300,55,40)
+
+    clock = pygame.time.Clock() # create clock
+    run = True
+    while run:
+        clock.tick(FPS) # set frames per seconf to counter
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # if x'd off it will quit
+                run = False
+
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed,yellow)
+        red_handle_movement(keys_pressed, red)
+
+        draw_window(red,yellow)
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
